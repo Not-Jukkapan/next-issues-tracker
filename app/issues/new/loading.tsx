cb2f498a877011944a7de5f4c5ec6a593981aaee
Jukkapan Kongjun <jukkapan.kn@gmail.com>
2024-10-09 14:23:02 +0700

    Refactoring - Organizing Imports

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,15 +1,14 @@
 import { Box } from '@radix-ui/themes'
 import React from 'react'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import { Skeleton } from '@/app/components'
 
 const LoadingNewIssuePage = () => {
-  return (
-    <Box className='max-w-xl'>
-        <Skeleton />
-        <Skeleton height={"20rem"}/>
-    </Box>
-  )
+    return (
+        <Box className='max-w-xl'>
+            <Skeleton />
+            <Skeleton height={"20rem"} />
+        </Box>
+    )
 }
 
 export default LoadingNewIssuePage
